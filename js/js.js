2268bbf85aea01be400b32e76a6fd60a7dbdd034
tayebmoradi <tayeb.mora69@gmail.com>
2023-06-13 23:14:29 +0330

    add total

--- js/js.js
+++ js/js.js
@@ -1,18 +1,37 @@
-
+debugger;
 function plus()
 {
-    document.getElementById('number').innerHTML++;
+   var test = document.getElementById('number').innerHTML++;
+    if(test)
+    {
+    var tot =parseFloat(document.getElementById('total').innerHTML) ;
+    
+    var total = tot + tot;
+    document.getElementById('total').innerHTML= total;
+
+    }
 }
 
 function mines()
 {
  var check = document.getElementById('number').innerHTML;
+ 
 if(check <= 1)
 {
    alert("عدد نمیتواند 0 و یا - باشد") 
 }
 else{
-    document.getElementById('number').innerHTML--;
+   var test = document.getElementById('number').innerHTML--;
+    if(test)
+    {
+    var tott =parseFloat(document.getElementById('total').innerHTML) ;
+    
+    var tota = tott - tott;
+    document.getElementById('total').innerHTML= tota;
+
+    }
 }
 }
+
+
 
